18ff0bc9431a2dd668b42971c26b4a73bbcd5fe7
Alexander Rukletsov <rukletsov@gmail.com>
2018-08-09 20:13:41 +0200

    Add __processes__ check

--- import/import.go
+++ import/import.go
@@ -3,6 +3,7 @@ package bun
 import (
 	_ "github.com/adyatlov/bun/check/dcosversion"
 	_ "github.com/adyatlov/bun/check/health"
+	_ "github.com/adyatlov/bun/check/mesos/actormailboxes"
 	_ "github.com/adyatlov/bun/check/nodecount"
 	_ "github.com/adyatlov/bun/file"
 )
